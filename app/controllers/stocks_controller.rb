c38f457a5ba51ecaa71a2e1ef583b6701d373b48
Christopher Briggs <christopher_w_briggs@hotmail.com>
2018-04-10 14:15:20 -0400

    added userstock tracking and associations

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -6,6 +6,7 @@ class StocksController < ApplicationController
 			@stock = Stock.new_from_lookup(params[:stock]) 
 			flash.now[:danger] = "Invalid Stock Symbol" unless @stock
 		end
+		
 		respond_to do |format|
 			format.js { render partial: 'users/result' }
 		end
